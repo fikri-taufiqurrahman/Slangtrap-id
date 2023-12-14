e9f38593b3f513ff2615a618684c5e68e5148863
Fikri Taufiqurrahman <133179864+fikri-taufiqurrahman@users.noreply.github.com>
2023-12-14 23:22:01 +0700

    Update run.py

--- ML/run.py
+++ ML/run.py
@@ -2,4 +2,4 @@ from application import app
 import os
 
 if __name__ == "__main__":
-    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
