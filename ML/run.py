71dcfcc798a6533bc78d193c2f9dd433cbb7df8c
widifaizakhmad <93120790+widifaizakhmad@users.noreply.github.com>
2023-12-14 22:54:49 +0700

    Update run.py
    Cloud Run traffic issues fix

--- ML/run.py
+++ ML/run.py
@@ -1,4 +1,5 @@
 from application import app
+import os
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8080,debug=True)
+    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
